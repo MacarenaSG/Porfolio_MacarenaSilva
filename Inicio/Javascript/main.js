bbf0ea36d2c6e320829544cc1761f5bdd50dcd4e
MacarenaSG <macarena.silvagarcia@gmail.com>
2025-07-14 21:53:45 +0200

    V12. Revisión media query para moviles

--- Inicio/Javascript/main.js
+++ Inicio/Javascript/main.js
@@ -13,3 +13,15 @@ function despliegaMenu(){
 }
 
 nav();
+
+const popup = document.getElementById("cookiePopup");
+const btn = document.getElementById("acceptCookies");
+
+if (!localStorage.cookiesAccepted){
+    popup.classList.add("show");
+}
+
+btn.addEventListener("click",function(){
+    localStorage.cookiesAccepted = "true";
+    popup.classList.remove("show");
+});
